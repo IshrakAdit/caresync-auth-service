1e8849a3d09d5d4c2f462f94cb514e241b769a0a
Ishrak-Adit07 <ishrak.adit07@gmail.com>
2025-05-18 19:56:51 +0600

    service impl: user service

--- server/src/main/java/com/caresync/service/auth/services/implementations/UserServiceImpl.java
+++ server/src/main/java/com/caresync/service/auth/services/implementations/UserServiceImpl.java
@@ -1,4 +1,19 @@
 package com.caresync.service.auth.services.implementations;
 
+import com.caresync.service.auth.dtos.request.RegistrationRequest;
+import com.caresync.service.auth.entities.User;
+import com.caresync.service.auth.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class UserServiceImpl {
+
+    private final UserRepository userRepository;
+
+//    @Override
+    public void registerUser(RegistrationRequest registrationRequest) {
+        userRepository.save(new User(registrationRequest.email(), registrationRequest.fullName(), registrationRequest.password(), registrationRequest.phoneNumber()));
+    }
 }
