36bab0c65a8e2d90fb81bc442bb0240bf40056fe
Ishrak-Adit07 <ishrak.adit07@gmail.com>
2025-05-19 12:03:52 +0600

    repository: user locations

---
+++ server/src/main/java/com/caresync/service/auth/repositories/UserLocationRepository.java
@@ -0,0 +1,7 @@
+package com.caresync.service.auth.repositories;
+
+import com.caresync.service.auth.entities.UserLocation;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserLocationRepository extends JpaRepository<UserLocation, Long> {
+}
--- server/src/main/java/com/caresync/service/auth/repositories/UserRepository.java
+++ server/src/main/java/com/caresync/service/auth/repositories/UserRepository.java
@@ -2,6 +2,7 @@ package com.caresync.service.auth.repositories;
 
 import com.caresync.service.auth.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
