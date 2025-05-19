fcf376f6c646444e3fe83a5442c60578676e73c4
Ishrak-Adit07 <ishrak.adit07@gmail.com>
2025-05-19 12:06:29 +0600

    update: dto - request - registration

--- server/src/main/java/com/caresync/service/auth/dtos/request/RegistrationRequest.java
+++ server/src/main/java/com/caresync/service/auth/dtos/request/RegistrationRequest.java
@@ -20,7 +20,18 @@ public record RegistrationRequest(
         )
         String password,
 
-        @Size(max = 255, message = "Location must be at most 255 characters long.")
-        String location
+        @NotBlank(message = "Address cannot be blank")
+        @Size(min = 2, max = 100, message = "Address must be between 2 and 100 characters")
+        String address,
 
+        String thana,
+
+        @NotBlank(message = "PO cannot be blank")
+        String po,
+
+        @NotBlank(message = "City cannot be blank")
+        String city,
+
+        @NotBlank(message = "Postal code cannot be blank")
+        String postalCode
 ) {}
