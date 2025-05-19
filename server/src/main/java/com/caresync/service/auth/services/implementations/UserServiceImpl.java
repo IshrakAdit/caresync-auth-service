63c3e43266c645b7341446430a769c492c8d5cc2
Ishrak-Adit07 <ishrak.adit07@gmail.com>
2025-05-19 12:15:33 +0600

    user service: register new user with location

--- server/src/main/java/com/caresync/service/auth/services/implementations/UserServiceImpl.java
+++ server/src/main/java/com/caresync/service/auth/services/implementations/UserServiceImpl.java
@@ -2,6 +2,8 @@ package com.caresync.service.auth.services.implementations;
 
 import com.caresync.service.auth.dtos.request.RegistrationRequest;
 import com.caresync.service.auth.entities.User;
+import com.caresync.service.auth.entities.UserLocation;
+import com.caresync.service.auth.repositories.UserLocationRepository;
 import com.caresync.service.auth.repositories.UserRepository;
 import com.caresync.service.auth.services.abstractions.UserService;
 import lombok.RequiredArgsConstructor;
@@ -12,14 +14,31 @@ import org.springframework.stereotype.Service;
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
+    private final UserLocationRepository userLocationRepository;
 
     @Override
     public void registerUser(RegistrationRequest registrationRequest) {
-        if(registrationRequest.location().isBlank()) {
+        if(registrationRequest.address() == null) {
             userRepository.save(new User(registrationRequest.name(), registrationRequest.email(), registrationRequest.password()));
         }
         else {
-            userRepository.save(new User(registrationRequest.name(), registrationRequest.email(), registrationRequest.password()));
+            User newUser = User.builder()
+                    .name(registrationRequest.name())
+                    .email(registrationRequest.email())
+                    .passwordHash(registrationRequest.password())
+                    .build();
+
+            UserLocation newUserLocation = new UserLocation(
+                    newUser, registrationRequest.address().address(),
+                    registrationRequest.address().thana(),
+                    registrationRequest.address().po(),
+                    registrationRequest.address().city(),
+                    registrationRequest.address().postalCode());
+
+            newUser.setLocation(newUserLocation);
+
+            userLocationRepository.save(newUserLocation);
+            userRepository.save(newUser);
         }
     }
 }
