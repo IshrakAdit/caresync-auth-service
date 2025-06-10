ec892961ea9fe336b6d1d80c1b7a849f93443681
IshrakAdit <ishrak.adit07@gmail.com>
2025-06-10 22:44:33 +0600

    config: firebase & jwt decoder

---
+++ server/src/main/java/com/caresync/service/auth/configs/FirebaseConfig.java
@@ -0,0 +1,34 @@
+package com.caresync.service.auth.configs;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+@Configuration
+public class FirebaseConfig {
+
+    @Bean
+    public FirebaseApp firebaseApp() throws IOException {
+        if (FirebaseApp.getApps().isEmpty()) {
+            InputStream serviceAccount = getClass().getClassLoader()
+                    .getResourceAsStream("firebase/firebase-adminsdk.json");
+
+            if (serviceAccount == null) {
+                throw new IOException("Firebase credential file not found in classpath.");
+            }
+
+            FirebaseOptions options = FirebaseOptions.builder()
+                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .build();
+
+            return FirebaseApp.initializeApp(options);
+        } else {
+            return FirebaseApp.getInstance();
+        }
+    }
+}
