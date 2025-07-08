58bde93eb1ed9ce9cafa8051b3bef1cf0b0b9c66
IshrakAdit <ishrak.adit07@gmail.com>
2025-07-08 18:35:36 +0600

    fix: sql query integrated user role

--- server/src/main/resources/db/migration/V1__initSchema.sql
+++ server/src/main/resources/db/migration/V1__initSchema.sql
@@ -3,11 +3,21 @@ CREATE TABLE users (
                        name VARCHAR(100) NOT NULL,
                        email VARCHAR(255) UNIQUE NOT NULL,
                        password_hash VARCHAR(255) NOT NULL,
+                       role VARCHAR(10) NOT NULL,
                        location_id BIGINT UNIQUE
 );
 
--- Insert sample users
-INSERT INTO users (id, name, email, password_hash, location_id)
+INSERT INTO users (id, name, email, password_hash, role, location_id)
 VALUES
-    ('user-1', 'Alice Ahmed', 'alice@example.com', 'hashedpassword1', null),
-    ('user-2', 'Bob Karim', 'bob@example.com', 'hashedpassword2', null);
+    ('user-1', 'Alice Ahmed', 'alice@example.com', 'hashedpassword1','DEFAULT', null),
+    ('user-2', 'Bob Karim', 'bob@example.com', 'hashedpassword2','DEFAULT', null),
+    ('user-3', 'Charlie Rahman', 'charlie@example.com', 'hashedpassword3', 'DEFAULT', null),
+    ('user-4', 'Dipa Sultana', 'dipa@example.com', 'hashedpassword4', 'DEFAULT', null),
+    ('user-5', 'Emran Hossain', 'emran@example.com', 'hashedpassword5', 'DEFAULT', null),
+    ('user-6', 'Farzana Khan', 'farzana@example.com', 'hashedpassword6', 'DEFAULT', null),
+    ('user-7', 'Gias Uddin', 'gias@example.com', 'hashedpassword7', 'DEFAULT', null),
+    ('user-8', 'Hasina Jahan', 'hasina@example.com', 'hashedpassword8', 'DEFAULT', null),
+    ('user-9', 'Imran Ali', 'imran@example.com', 'hashedpassword9', 'DEFAULT', null),
+    ('user-10', 'Jannat Akter', 'jannat@example.com', 'hashedpassword10', 'DEFAULT', null),
+    ('user-11', 'Kamal Mia', 'kamal@example.com', 'hashedpassword11', 'DEFAULT', null),
+    ('user-12', 'Lamia Ferdous', 'lamia@example.com', 'hashedpassword12', 'DEFAULT', null);
